4b50d21a0a0bb9f0487b94b83615a4245f368a8f
Maxim Ng <maximng000@users.noreply.github.com>
2026-04-20 02:40:57 +0800

    fix: handle error from os.Pipe in captureOutput

--- cmd/helpers_test.go
+++ cmd/helpers_test.go
@@ -7,17 +7,21 @@ import (
 )
 
 // captureOutput runs fn and returns everything written to stdout.
+// It temporarily replaces os.Stdout with a pipe to capture output.
 func captureOutput(fn func() error) (string, error) {
 	old := os.Stdout
-	r, w, _ := os.Pipe()
+	r, w, err := os.Pipe()
+	if err != nil {
+		return "", err
+	}
 	os.Stdout = w
 
-	err := fn()
+	fnErr := fn()
 
 	w.Close()
 	os.Stdout = old
 
 	var buf bytes.Buffer
 	io.Copy(&buf, r)
-	return buf.String(), err
+	return buf.String(), fnErr
 }
